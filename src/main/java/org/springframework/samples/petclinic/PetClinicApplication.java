1d534a2f92ed933bb41747ad9cbe856dd1d3c312
Sujal <128172175+sujal-malviya@users.noreply.github.com>
2026-01-20 15:10:58 +0530

    new

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -1,9 +1,10 @@
 package org.springframework.samples.petclinic;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-// @SpringBootApplication
+@SpringBootApplication
 public class PetClinicApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
